a7dc1631c7b0cd34f2ec3700ae1556cead799ef3
lukabaca <lkszblaszczyk@gmail.com>
2018-05-28 18:37:47 +0200

    basic structure

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                XmlReader xmlReader = new XmlReader("example.xml");
+                xmlReader.testReading();
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("File was not found");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Linq/Linq/XmlReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Linq
+{
+    class XmlReader
+    {
+        private String xmlFileName;
+        private XmlTextReader reader;
+
+        public XmlReader(String fileName)
+        {
+            this.reader = new XmlTextReader(fileName);
+            this.xmlFileName = fileName;
+        }
+
+        public void testReading()
+        {
+            while(reader.Read())
+            {
+                Console.WriteLine(reader.Name);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
